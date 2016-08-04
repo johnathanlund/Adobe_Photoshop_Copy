dddb63e0d9694477696f32c5ae4bac92fbdf97cb
Johnathan Lund <jalshield@gmail.com>
2016-08-03 23:15:38 -0600

    working on ipad size media queries.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).scroll(function() {
 //detect when user scroll to top and set position to relative else sets position to fixed
 $("#scrolling_Header").css({
 "top": "0",
-"position": $(this).scrollTop() > 140 ? "fixed" : "relative"
+"position": $(this).scrollTop() > 140 ? "fixed" : "relative",
+"background": "#131630",
+"width": "100%"
 });
 });
